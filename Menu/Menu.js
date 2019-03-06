0673c67fd95e0b56dd399015cf9484669a560a8d
josephriosio <blankworldwide@gmail.com>
2019-03-06 12:56:09 -0800

    completed one part of stretch

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,34 @@
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle("menu--open");
-};
+// const toggleMenu1 = () => {
+//   // Toggle the "menu--open" class on your menu refence.
+//   menu.classList.toggle("menu--open");
+// };
+//
+// // Start Here: Create a reference to the ".menu" class
+// // create a reference to the ".menu-button" class
+//
+// // Using your menuButton reference, add a click handler that calls toggleMenu
+// menuBtn.addEventListener("click", toggleMenu1);
 
-// Start Here: Create a reference to the ".menu" class
+// Select Dom Items
+const menuBtn = document.querySelector(".menu-button");
 const menu = document.querySelector(".menu");
-console.log(menu);
-// create a reference to the ".menu-button" class
-const menuButton = document.querySelector(".menu-button");
-// Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", toggleMenu);
+
+//set initial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menu.classList.toggle("menu--open");
+    //show menu state to true
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menu.classList.remove("menu--open");
+    // set menu back to false
+    showMenu = false;
+  }
+}
